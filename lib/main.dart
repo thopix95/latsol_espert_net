1e47a3b857ec41a5c72acd22f99c65c38b28474a
thopix95 <thopix95@gmail.com>
2022-04-26 13:36:58 +0700

    menambah splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latsol/view/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: SplashScreen(),
+    );
   }
 }
