de17096772f0ce3d4986081dedfed3bf389cdd91
thopix95 <thopix95@gmail.com>
2022-04-26 15:32:00 +0700

    tambahan loginpage dan timer navigator

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,10 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:latsol/view/login_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Timer(
+        Duration(
+          seconds: 5,
+        ), () {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => LoginPage()));
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.indigoAccent,
+      body: Center(
+        child: Image.asset("assets/auth/ic_splash.png"),
+      ),
+    );
   }
 }
