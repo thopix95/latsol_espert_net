a5b50c1038c2594e4c0457fe9eb05c0e0d0b0477
thopix95 <thopix95@gmail.com>
2022-04-26 11:23:09 +0700

    tambhan class

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
